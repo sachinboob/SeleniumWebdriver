a510c28748eb9b925b9932b96306978ea52de973
sachinboob <boob.sachin@gmail.com>
2018-06-09 21:06:23 +0530

    DemoApplicationSelenium - Removed unused imports

--- src/com/demo/DemoApplicationSelenium.java
+++ src/com/demo/DemoApplicationSelenium.java
@@ -6,16 +6,12 @@ import static com.driver.utils.ElementFinder.getElementWithWait;
 import java.io.File;
 import java.util.Calendar;
 import java.util.Iterator;
-import java.util.TimeZone;
 
 import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.driver.utils.Browser;
 import com.driver.utils.ElementFinder;
